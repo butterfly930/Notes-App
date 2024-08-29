31419739d1979212bb561c2295f389ce53475612
butterfly930 <elsarrustaj@gmail.com>
2024-08-29 16:19:09 +0200

    commit

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import AddEditNotes from './AddEditNotes';
 import { useNavigate } from "react-router-dom";
 import Modal from 'react-modal';
 import axiosInstance from '../../utils/axiosInstance';
+import Toast from '../../components/ToastMessage/Toast';
 
 
 const Home = () => {
@@ -16,11 +17,34 @@ const Home = () => {
     data: null,
   });
 
+  const [showToastMsg, setShowToastMsg] = useState ({
+    isShown: false,
+    message: "",
+    type: "add",
+  });
+
   const [allNotes, setAllNotes] = useState([])
   const [userInfo, setUserInfo] = useState(null);
 
   const navigate = useNavigate();
 
+  const handleEdit = (noteDetails) => {
+    setOpenAddEditModal({isShown: true, data: noteDetails, type: "edit"});
+  };
+
+  const showToastMessage = (message, type) => {
+    setShowToastMsg({
+      isShown: false,
+      message,
+      type, 
+    });
+  };
+  const handleCloseToast = () => {
+    setShowToastMsg({
+      isShown: false,
+      message:"",
+    });
+  };
   //Get User Info 
   const getUserInfo = async () => {
     try {
@@ -61,16 +85,20 @@ const Home = () => {
 
     <div className="container mx-auto">
       <div className="grid grid-cols-3 gap-4 mt-8">
-      <NoteCard 
-        title="Meeting on 7th April" 
-        date="3rd Apr 2024" 
-        content="Meeting on 7th April" 
-        tags="#Meeting"
-        isPinned={true}
-        onEdit={()=>{}}
-        onDelete={()=>{}}
-        onPinNote={()=>{}}
-      />
+        {allNotes.map((item, index)=>(
+          <NoteCard 
+          key={item._id}
+          title={item.title}
+          date={item.createdOn}
+          content={item.content}
+          tags={item.tags}
+          isPinned={item.isPinned}
+          onEdit={()=>handleEdit(item)}
+          onDelete={()=>{}}
+          onPinNote={()=>{}}
+        />
+        ))}
+      
       </div>
     </div>
 
@@ -104,8 +132,17 @@ const Home = () => {
     onClose={() => {
       setOpenAddEditModal({isShown:false, type:"add", data:null });
     }}
+    getAllNotes={getAllNotes}
+    showToastMessage={showToastMessage}
     />
     </Modal>
+
+    <Toast
+      isShown={showToastMsg.isShown}
+      message={showToastMsg.message}
+      type={showToastMsg.type}
+      onClose={handleCloseToast}
+      />
     </>
   );
 };
